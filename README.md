93f4f17c9d1387d0dcb81edb82fa07b176533fd3
François Beausoleil <francois@teksol.info>
2013-04-15 16:48:44 -0400

    Pre-download chronosg

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ Vagrant Chronos
 
 This is a Vagrant repository with the necessary packages to boot Chronos. I
 had some difficulties compiling Mesos, and the Vagrantfile helps with that.
+
+Chronos is pre-cloned into ~/chronos. To install, run:
+
+    vagrant ssh
+    cd chronos
+    bin/installer.bash
--- manifests/base.pp
+++ manifests/base.pp
@@ -2,17 +2,27 @@ exec{'apt-get update':
   command => '/usr/bin/apt-get update',
 }
 
-package{ [
-    'vim-nox',
-    'openjdk-6-jre-headless',
-    'openjdk-6-jdk',
-    'build-essential',
-    'curl',
-    'git',
-    'autoconf',
-    'libtool',
-    'libcurl3-dev',
-  ]:
+$packages = [
+  'vim-nox',
+  'openjdk-6-jre-headless',
+  'openjdk-6-jdk',
+  'build-essential',
+  'curl',
+  'git',
+  'autoconf',
+  'libtool',
+  'libcurl3-dev',
+]
+
+package{ $packages :
   ensure => latest,
   require => Exec['apt-get update'],
+}
+
+$chronos_already_installed = 'test -d /home/vagrant/chronos'
+
+exec{'git clone chronos':
+  command => '/usr/bin/git clone git://github.com/airbnb/chronos.git /home/vagrant/chronos && /bin/chmod -R vagrant:vagrant /home/vagrant/chronos',
+  require => Package[ $packages ],
+  unless  => $chronos_already_installed,
 }
